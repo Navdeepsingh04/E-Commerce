b3e56447996b5d995411c3c29fc9b3042a939125
Navdeepsingh04 <88830033+Navdeepsingh04@users.noreply.github.com>
2023-09-12 22:07:16 +0530

    productSlice

---
+++ src/Pages/ProductDetailPage.js
@@ -0,0 +1,19 @@
+import NavBar from "../features/navBar/NavBar";
+import ProductDetail from "../features/projectList/components/ProductDetails";
+
+
+
+
+
+function ProductDetailPage() {
+    return ( 
+        <div>
+        <NavBar>
+          <ProductDetail></ProductDetail>
+        </NavBar>
+
+        </div>
+     );
+}
+
+export default ProductDetailPage ;
