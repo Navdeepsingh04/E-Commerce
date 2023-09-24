329a6d77e46dd0ed0300e230ed0bbd14f4793490
Navdeepsingh04 <88830033+Navdeepsingh04@users.noreply.github.com>
2023-09-25 00:29:41 +0530

    second

--- src/features/project/productAPI.js
+++ src/features/project/productAPI.js
@@ -1,7 +1,8 @@
 // A mock function to mimic making an async request for data
-export  function fetchCount(amount = 1) {
+export  function fetchAllProducts() {
   return new Promise(async(resolve) =>{
-    const response =await fetch('http://localhost:8080')
+    //Here We Will not hardcode the server URL.
+    const response =await fetch('http://localhost:8080/products')
     const data =response.json()
     resolve({data})}
   );
